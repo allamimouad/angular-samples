026ff38b103b3f9f75d88dabc5169b99fb02c1ac
allamimouad <allamimouad@gmail.com>
2022-10-13 21:57:41 +0200

    chapter9 section 9.1 (A high-level overview of change detection)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+
+  constructor(){
+    setTimeout(() => { this.title = "angular 2" }, 2000)
+  }
 }
